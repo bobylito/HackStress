158dea28dc5d1cce2a160c98174fc3ff1b699584
Adrien Maillol <ama@zenexity.com>
2012-06-28 03:57:46 +0200

    add project name filter

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,7 +1,12 @@
 $(function() {
+	$.urlParam = function(name){
+	    var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(window.location.href);
+	    return results[1] || 0;
+	}
+
 	if(!!window.EventSource) {
 		
-		var source = new EventSource("/live");
+		var source = new EventSource("/live/"+$.urlParam('project'));
 		
 		source.onmessage =  function(e) {
 			var msg = JSON.parse(e.data);
