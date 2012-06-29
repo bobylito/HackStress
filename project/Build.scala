14df0e10adec3b4270ba3c546dd533a89b235367
Jacques <jba@zenexity.com>
2012-06-29 11:46:18 +0200

    Cagetteeee

--- project/Build.scala
+++ project/Build.scala
@@ -13,7 +13,7 @@ object ApplicationBuild extends Build {
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
+    	resolvers += ("Guillaume Bort" at "http://guillaume.bort.fr/repository/")
     )
 
 }
