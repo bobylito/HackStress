8b69e2fc87339eb59e501409d13486e451d74b62
Alexandre Stanislawski <bobylito@gmail.com>
2012-06-29 16:25:33 +0200

    Update client view

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,16 +1,41 @@
 $(function() {
-	function urlParam(name){
-	    var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(window.location.href) || 0;
-	    return results[1];
-	}
 
-	if(!!window.EventSource) {
-		var source = new EventSource("/live/"+urlParam('project'));
-		
-		source.onmessage =  function(e) {
-			var msg = JSON.parse(e.data);
-			$("#messages").append("<li>" + JSON.stringify( msg ) + "</li>")
-		};
-		
-	}
+  var msgTemplate = _.template($("#message_template").html()),
+      urlParam = function(name) {
+        var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(window.location.href) || 0;
+        return results[1];
+      },
+      bindEvtSrc = function(projectName){
+        if(!!window.EventSource) {
+          var source = new EventSource("/live/"+projectName);
+
+          source.onmessage =  function(e) {
+            var msg = JSON.parse(e.data);
+            msg.date = new Date().toLocaleDateString();
+            $("#messages").append( msgTemplate( msg ) )
+          };
+        }
+      },
+      initProjectName = urlParam('project');
+
+  if(initProjectName){
+    $('#tellMe').hide(); 
+    bindEvtSrc(initProjectName);
+  }
+  else {
+    $('#tellMe').submit(function(e){
+      e.preventDefault();
+      e.stopPropagation();
+      var projet = $("#project").val();
+
+      history.pushState({}, document.title, location.href+"?project="+projet);
+
+      bindEvtSrc(projet)
+
+      $('#tellMe').hide(); 
+    });
+  }
+
+  
+
 });
